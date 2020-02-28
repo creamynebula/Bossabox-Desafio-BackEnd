38fabee2c5e66aace3cb7e9ea71865605ebd50a6
creamynebula <mateus.souzaaa@gmail.com>
2020-02-28 11:56:32 -0300

    iniciei o servidor e comecei a servir static files via express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 let express = require('express')
+let app = express(
+
+)
+
+app.use(express.static('public'))  //os static files vem de root/public
+
+const PORT = process.env.PORT || 3000 //process environment PORT ou 3000
+
+app.listen(PORT, () => console.info(`Servidor rodando na porta ${PORT}`)) //os backticks é pra poder colocar variável na string
