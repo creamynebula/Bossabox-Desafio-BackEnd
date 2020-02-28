b5d23032244da10089f158d37005a77a67bd99ce
creamynebula <mateus.souzaaa@gmail.com>
2020-02-28 13:07:44 -0300

    rota no person.js, chamar ela no index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let express = require('express')
-let app = express(
+let app = express()
 
-)
+let personRoute = require('./routes/person')  //local da rota, note que não precisa colocar .js
 
+app.use(personRoute)  //dizendo ao express pra usar personRoute
 app.use(express.static('public'))  //os static files vem de root/public
 
 const PORT = process.env.PORT || 3000 //process environment PORT ou 3000
--- src/routes/person.js
+++ src/routes/person.js
@@ -3,5 +3,11 @@ let router = express.Router()
 
 
 
+//rota criada que pode ser acessada em localhost:PORT/person
+router.get('/person', (req, res) => {  //(req, res) = 'request' e 'response'
+    res.send('Você pediu uma pessoa.')   //envia uma mensagem dizendo o que o user pediu
+})  //call o object 'person'
 
-module.exports = router
+
+
+module.exports = router  //agora pode importar o router no index.js
