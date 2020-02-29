523c3928d298e3cf7c3fb2b53da7389781d16855
creamynebula <mateus.souzaaa@gmail.com>
2020-02-29 19:17:13 -0300

    queries

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let express = require('express')
 let app = express()
 
-let personRoute = require('./routes/person')  //local da rota, note que não precisa colocar .js
-
-app.use(personRoute)  //dizendo ao express pra usar personRoute
 app.use(express.static('public'))  //os static files vem de root/public
+
+let personRoute = require('./routes/person')  //local da rota, note que não precisa colocar .js
+app.use(personRoute)  //dizendo ao express pra usar personRoute
+
 
 const PORT = process.env.PORT || 3000 //process environment PORT ou 3000
 
--- src/routes/person.js
+++ src/routes/person.js
@@ -4,10 +4,25 @@ let router = express.Router()
 
 
 //rota criada que pode ser acessada em localhost:PORT/person
+// QueryString => Query propriedade no objeto 'req' (request)
+//ex: localhost:3000/person?name=chihaya&age=16
+//as query são key=value, o exemplo tem 2 queries
 router.get('/person', (req, res) => {  //(req, res) = 'request' e 'response'
-    res.send('Você pediu uma pessoa.')   //envia uma mensagem dizendo o que o user pediu
+    if (req.query.name) {  //se a query tem um nome
+        res.send(`Você pediu a pessoa chamada ${req.query.name}`)
+    }
+    else {
+        res.send('Você pediu uma pessoa.')
+    }
+    
 })  //call o object 'person'
 
+//subrota de 'person' que vai ser mapeada para a variável 'name'
+//propriedade 'params' no objeto 'req' (request)
+//localhost:3000/person/chihaya
+router.get('/person/:name', (req, res) => {  //(req, res) = 'request' e 'response'
+    res.send(`Você pediu a pessoa chamada ${req.params.name}`)   //envia uma mensagem dizendo o que o user pediu
+})
 
 
 module.exports = router  //agora pode importar o router no index.js
