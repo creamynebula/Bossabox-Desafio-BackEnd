778f1bca4545b35f0eda452b62d3e4eaf4d7bc98
creamynebula <mateus.souzaaa@gmail.com>
2020-03-01 09:13:30 -0300

    lidar com erros 404

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 let express = require('express')
-let app = express()
-
+let app = express() 
 
 
 let personRoute = require('./routes/person')  //local da rota, note que não precisa colocar .js
@@ -13,6 +12,17 @@ app.use((req, res, next) => {
 app.use(personRoute)  //dizendo ao express pra usar personRoute
 app.use(express.static('public'))  //os static files vem de root/public
 
+// lidar com erro 404
+app.use((req, res, next) => {
+    res.status(404).send('Aonde você está tentando ir?')
+
+})
+
+// lidar com erro 500 (internal server error)
+app.use((err, req, res, next) => { //essa função recebe um error object 'err' também
+    console.error(err.stack)
+
+})
 
 const PORT = process.env.PORT || 3000 //process environment PORT ou 3000
 
