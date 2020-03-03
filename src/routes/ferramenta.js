792e58ddef8058adfedba1792ccf0a35be017013
creamynebula <mateus.souzaaa@gmail.com>
2020-03-03 15:49:22 -0300

    code cleanup e tentativa de fazer o POST ferramenta retornar algo dps de adicionar ao DB, no success so far.

--- src/routes/ferramenta.js
+++ src/routes/ferramenta.js
@@ -30,42 +30,21 @@ router.get('/error', (req, res) => {
 })
 
 //criar uma nova ferramenta via POST pra localhost:3000/ferramenta
-router.post('/ferramenta', (req, res) => {
-    if(!req.body) { //req.body existe graças ao body-parser
-        //retorna status 400 (bad request) e manda uma msg
-        return res.status(400).send('Sem corpo no request T_T erro 66256')
+router.post('/ferramenta', async (req, res) => {
+    if (!req.body) {
+      return res.status(400).send('Sem corpo no request T_T erro 66256');
     }
-
-    //if(!req.body.title) {} só pra mostrar que daria pra lidar com a ausência de qualquer field
-    //lembrando que o Model já tá lidando com algumas validações pra gente (com type, required etc)
-    //se alguma delas der ruim, vai pro .catch statement
-
-    /*
-    exemplo de ferramenta, é isso que vai no req.body
-    let tool1 = {
-	"id": 2,
-	"title": "Martelo de Ouro",
-	"link": "goldenhammer.io",
-	"description": "Um martelo gigante de ouro!",
-	"tags": [
-		"7m de comprimento",
-		"puro ouro"
-	]}
-    */
-
-    //se tem body, vamos criar uma ferramenta e add no DB
-    let model = new FerramentaModel(req.body)
-    return model.save()  //isso vai mandar pro DB (mongoose -> mongodriver -> DB)
-        .then(doc => {  //depois que salvamos: com o doc que foi saved (doc é um array de documents)
-            if(!doc || doc.length === 0) { //se não tem doc
-                res.status(500).send(doc)
-            }
-            //se tem doc -> status 201 = resource was created
-            res.status(201)
-        })
-        .catch(err => {  //lidar com erros
-            res.status(500).json(err)
-        })
-})
+  
+    try {
+      const model = new FerramentaModel(req.body);
+      const doc = await model.save();
+      if (doc || doc.length) {
+        return res.status(201);
+      }
+      return res.status(500).send(doc);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  })
 
 module.exports = router
